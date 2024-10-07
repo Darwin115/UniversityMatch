9710d1f1fe994fef6111b1f2022f512f28f90787
LuzAneliCampos <21151603@aguascalientes.tecnm.mx>
2024-10-06 21:39:03 -0600

    Acomodo

--- scriptnav.js
+++ scriptnav.js
@@ -1,9 +1,7 @@
 function selectLink(selectedLink) {
     var links = document.querySelectorAll("#navbar li a");
-    
     links.forEach(function(link) {
         link.classList.remove("active");
     });
-    
     selectedLink.classList.add("active");
 }
