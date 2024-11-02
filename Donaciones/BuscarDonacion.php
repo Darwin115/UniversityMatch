4d1614cc96553b2cdbcc221fb07b007b8c4cfc08
LuzAneliCampos <21151603@aguascalientes.tecnm.mx>
2024-11-01 20:42:34 -0600

    Donaciones

--- Donaciones/BuscarDonacion.php
+++ Donaciones/BuscarDonacion.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>MultiShop - Online Shop Website Template</title>
+    <title>Donaciones</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
@@ -15,30 +15,53 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick-theme.css" />
     <script src="./scriptnav.js"></script>
-    <link href="img/favicon.ico" rel="icon">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
 </head>
 
 <body>
     <?php include '../header.php'; ?>
-    <div class="productos">
-        <div class="filters-container">
-        <select id="filtrar">
-            <option value="ninguno">Filtrar por categoría</option>
-            <option value="soc">Ciencias Sociales</option>
-            <option value="bio">Ciencias Biológicas</option>
-            <option value="fm">Ciencias Físico-Matemáticas</option>
-            <option value="hum">Arte y Humanidades</option>
-        </select>
-        <input type="text" id="filtro" placeholder="Buscar por nombre">
-        <button id="buscar">Buscar</button>
+    <!-- Bottom Bar Start -->
+    <div class="bottom-bar">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="productos">
+                        <div class="filters-container">
+                            <select id="filtrar">
+                                <option value="ninguno">Filtrar por categoría</option>
+                                <option value="soc">Ciencias Sociales</option>
+                                <option value="bio">Ciencias Biológicas</option>
+                                <option value="fm">Ciencias Físico-Matemáticas</option>
+                                <option value="hum">Arte y Humanidades</option>
+                            </select>
+                            <input type="text" id="filtro" placeholder="Buscar por nombre">
+                            <button id="buscar">Buscar</button>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="user">
+                            <a href="wishlist.html" class="btn wishlist">
+                                <i class="fa fa-heart"></i>
+                                <span>(0)</span>
+                            </a>
+                            <a href="cart.html" class="btn cart">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span>(0)</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+        <!-- Bottom Bar End -->     
 
     <p class="text-center">Explora nuestras donaciones disponibles a continuación. Si necesitas algo específico, ¡contáctanos y te ayudaremos a encontrarlo!</p>
     <span>&nbsp;&nbsp;&nbsp;</span>
@@ -51,7 +74,7 @@
     <script src="/script.js"></script>
     <script src="Donaciones.js"></script>
 
-    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+    <a href="#" class="bt custom-btn back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
